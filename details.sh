47d8ca641a6ef9165900fe6f9145421abc831b4b
testuser1 <testuser2@ip-10-1-1-24.ec2.internal>
2023-04-28 07:02:37 +0000

    add sudo code in the script

--- details.sh
+++ details.sh
@@ -1,6 +1,10 @@
 
 #!/bin/bash
 echo "this script will provid information about the local system"
+#Gives hostname
 echo $hostname
+#gives user excuted this script
 echo $(whoami)
-echo $(id -un)
+#GIVES LIST OF users in the system
+USERS=$cat /etc/passwd |cut -d ":" -f 1
+echo $USERS
